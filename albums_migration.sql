7cea8d2f0e358ee1c0cfa3308562ec2343a086ca
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-23 10:35:53 -0500

    Completed Excercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,12 @@
 USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist_name VARCHAR(100) NOT NULL,
+    release_date  DATE,
+    sales DECIMAL,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
