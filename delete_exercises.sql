6f781d69bbe5c4a0ef3f94862ac196be230d9178
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-23 15:05:43 -0500

    Completed Excercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,9 +1,9 @@
 USE codeup_test_db;
 
-SELECT name
+DELETE
 FROM albums
-WHERE release_date > '1991';
-# Album names that were released after 1991
+WHERE release_date BETWEEN '1992' AND '2021';
+# Delete albums that were released after 1991
 
 DELETE
 FROM albums
