885af94f489479a8d7826e754fc1115881f33de3
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-23 11:12:06 -0500

    More Updates...

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,11 +2,13 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE IF NOT EXISTS albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_name VARCHAR(100) NOT NULL,
-    release_date  DATE,
-    sales DECIMAL,
-    genre VARCHAR(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(250) NOT NULL,
+    name         VARCHAR(250) NOT NULL,
+    release_date DATE,
+    sales        FLOAT,
+    genre        VARCHAR(100),
     PRIMARY KEY (id)
 );
