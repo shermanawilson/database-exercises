373a01819752e7ece4b974094384c4a0fe062323
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-23 14:42:51 -0500

    More Updates... I'm taking a break

---
+++ update_exercises.sql
@@ -0,0 +1,30 @@
+USE codeup_test_db;
+
+
+# A.
+SELECT *
+FROM albums;
+
+# B.
+SELECT *
+FROM albums
+WHERE release_date < '1980';
+
+# C.
+SELECT *
+FROM albums
+WHERE artist = 'Michael Jackson';
+
+# A.1
+UPDATE albums
+SET sales = sales * 10;
+# Multiples all sales by 10
+
+# B.1
+
+
+# C.1
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE id = 1;
+#  Changes Michael Jackson to Peter Jackson
