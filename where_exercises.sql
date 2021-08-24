8953dc6be77954ae20d5a61fcbe2935d3d7968e7
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-24 11:48:13 -0500

    Finished

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,50 +1,47 @@
 USE employees;
 
 # Selects all employees with the First Names Irena, Vidya and Maya
-SELECT emp_no, first_name, last_name
+SELECT COUNT(*)
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 
 # All employees that last name start with E
-SELECT last_name
-FROM employees.employees
-         AS E
-WHERE E.last_name LIKE 'e%';
+SELECT COUNT(*)
+FROM employees
+WHERE last_name LIKE 'e%';
 
 
 # All employees with a q in their last name
-SELECT last_name
-FROM employees.employees
-         AS E
-WHERE E.last_name LIKE '%q%';
+SELECT COUNT(*)
+FROM employees
+WHERE last_name LIKE '%q%';
 
 
 # Find all first names with Irena , Vidya and Maya
-SELECT emp_no, first_name, last_name, gender
-FROM employees.employees
-         AS E
+SELECT COUNT(*)
+FROM employees
 WHERE gender = 'M'
-   AND (first_name = 'Irena'
-       OR first_name = 'Vidya'
-       OR first_name = 'Maya'
+  AND (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya'
     );
 
 
 # All employees who last name starts or ends with E
-SELECT last_name
-FROM employees.employees
-         AS E
-WHERE E.last_name LIKE 'e%%e';
+SELECT COUNT(*)
+FROM employees
+WHERE last_name LIKE '%e'
+   OR last_name LIKE 'e%';
 
 
 # All employees who last name starts and ends with E
-SELECT last_name
-FROM employees.employees
-         AS E
-WHERE E.last_name LIKE 'e%e';
+SELECT COUNT(*)
+FROM employees
+WHERE last_name LIKE 'e%e';
 
 # All employees with a q in their last name but not a qu
-SELECT last_name
+SELECT COUNT(*)
 FROM employees
-WHERE last_name NOT LIKE '%qu%' and last_name LIKE '%q%' ;
+WHERE last_name NOT LIKE '%qu%'
+  AND last_name LIKE '%q%';
