84f2cac11f9619a8e8cf15b867da46b0e8f368d5
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-24 09:45:02 -0500

    Completed Excercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -21,6 +21,14 @@ SET sales = sales * 10;
 # Multiples all sales by 10
 
 # B.1
+UPDATE albums
+SET release_date = 2021
+WHERE release_date < 1980;
+
+
+SELECT *
+FROM albums
+WHERE release_date = 2021;
 
 
 # C.1
@@ -28,3 +36,7 @@ UPDATE albums
 SET artist = 'Peter Jackson'
 WHERE id = 1;
 #  Changes Michael Jackson to Peter Jackson
+
+SELECT *
+FROM albums
+WHERE artist = 'Peter Jackson';
