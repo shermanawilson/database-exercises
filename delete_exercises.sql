81ca91283745691ee70a5a614e485b600acbbcba
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-24 09:52:34 -0500

    Finished

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,17 +2,17 @@ USE codeup_test_db;
 
 DELETE
 FROM albums
-WHERE release_date BETWEEN '1992' AND '2021';
+WHERE release_date > 1991;
 # Delete albums that were released after 1991
 
 DELETE
 FROM albums
-WHERE id = 7;
+WHERE genre = 'Disco';
 # Deleted the disco album
 
 DELETE
 FROM albums
-WHERE id = 4;
+WHERE artist = 'Whitney Houston';
 #  Deleted the Whitney album
 
 SELECT *
