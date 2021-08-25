62c14eab2b67fbec9da21deda858a164642be479
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-25 09:25:49 -0500

    Almost doneee...

---
+++ functions_exercises.sql
@@ -0,0 +1,27 @@
+USE employees;
+
+# All employees who last name starts and ends with E, Concat first and last names together
+SELECT CONCAT(first_name, last_name)
+FROM employees
+WHERE last_name LIKE 'e%e';
+
+# Select all employees who's birthday on christmas
+SELECT COUNT(*)
+FROM employees
+WHERE MONTH(birth_date) = (12) AND DAY(birth_date) = (25);
+
+
+# All employees hired in the 90's born on Christmas
+SELECT COUNT(*)
+FROM employees
+WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = (12) AND DAY(birth_date) = (25);
+
+
+# Employees Hired on Christmas in the 90's on Christmas, Oldest employee hired
+SELECT first_name, last_name
+FROM employees
+WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = (12) AND DAY(birth_date) = (25)
+ORDER BY hire_date DESC LIMIT 1;
+
+
+
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -14,3 +14,36 @@ ORDER BY salary DESC LIMIT 5;
 SELECT emp_no
 FROM salaries
 ORDER BY salary DESC LIMIT 5 OFFSET 45;
+
+
+
+SELECT CONCAT('Hello ', 'Codeup', '!')
+AS WelcomeMessage;
+
+
+SELECT DAYOFMONTH('1990-05-03');
+
+SELECT DAY('2017-03-25');
+
+SELECT MONTH('2008-02-03');
+
+SELECT YEAR('1987-01-01');
+
+SELECT NOW();
+# YYYY-MM-DD HH:MM:SS
+
+SELECT CURDATE();
+# YYYY-MM-DD
+
+SELECT CURTIME();
+# HH:MM:SS
+
+SELECT *
+FROM employees;
+
+SELECT  UNIX_TIMESTAMP() urrentTimeInUnix
+
+SELECT CONCAT(first_name, ' ', last_name,'s birthday is ',MONTHNAME(birth_date))
+AS PeoplesBirthdays
+FROM employees
+LIMIT 10;
