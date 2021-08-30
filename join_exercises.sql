80a6f92d3be74ecdc3540e0722c4f6eca8120532
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-08-30 11:19:49 -0500

    Update

--- join_exercises.sql
+++ join_exercises.sql
@@ -93,7 +93,6 @@ FROM employees AS E
 WHERE E.emp_no = 10001;
 
 
-
 # Grab all dept names FOr everyone who's last name starts with D
 SELECT E.first_name, E.last_name, dept_emp.from_date, dept_emp.to_date, D.dept_name
 FROM employees AS E
@@ -115,8 +114,59 @@ WHERE E.emp_no = 10001;
 
 #  WALK THROUGH
 
+# 1
+
+SELECT d.dept_name                        AS `Department Name`,
+       CONCAT(first_name, ' ', last_name) AS `Department Manager`
+FROM departments d
+         INNER JOIN dept_manager dm ON d.dept_no = dm.dept_no
+         INNER JOIN employees e ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01';
+
+# 2
+
+SELECT d.dept_name                        AS `Department Name`,
+       CONCAT(first_name, ' ', last_name) AS `Department Manager`
+FROM departments d
+         INNER JOIN dept_manager dm ON d.dept_no = dm.dept_no
+         INNER JOIN employees e ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01'
+  AND e.gender = 'F';
+
+# 3
+
+SELECT t.title,
+       COUNT(t.emp_no)
+           AS Title
+FROM departments d
+         INNER JOIN dept_emp de on d.dept_no = de.dept_no
+         INNER JOIN titles t ON de.emp_no = t.emp_no
+WHERE d.dept_name = 'Customer Service'
+  AND de.to_date = '9999-01-01'
+  AND t.to_date = '9999-01-01'
+GROUP BY t.title;
 
 
+# 4
+SELECT d.dept_name                        AS `Department Name`,
+       CONCAT(first_name, ' ', last_name) AS `Department Manager`,
+       s.salary                           AS Salary
+FROM departments d
+         INNER JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+         INNER JOIN employees AS e ON dm.emp_no = e.emp_no
+         INNER JOIN salaries AS s on e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01'
+  AND s.to_date = '9999-01-01';
 
+# Bonus
 
-
+SELECT CONCAT(e.first_name, ' ', e.last_name)       AS Employee,
+       d.dept_name                              AS Department,
+       CONCAT(e2.first_name, ' ', e2.last_name) AS Manager
+FROM departments AS d
+         INNER JOIN dept_emp AS de on d.dept_no = de.dept_no
+         INNER JOIN employees AS e on de.emp_no = e.emp_no
+         INNER JOIN dept_manager AS dm on d.dept_no = dm.dept_no
+         INNER JOIN employees AS e2 ON dm.emp_no = e2.emp_no
+WHERE de.to_date = '9999-01-01'
+  AND dm.to_date = '9999-01-01';
